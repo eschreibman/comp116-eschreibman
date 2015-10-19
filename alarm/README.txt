8e2aa0631f6893a2aa12466d9c043e213a159140
eschreibman <eliza.schreibman@gmail.com>
2015-10-18 20:28:14 -0400

    working live and log alarm detection

--- alarm/README.txt
+++ alarm/README.txt
@@ -1,14 +1,26 @@
-Identify what aspects of the work have been correctly implemented and what have not.
-Identify anyone with whom you have collaborated or discussed the assignment.
-Aaron Bowen
-Becky Cutler
-Daniel Baigel
-Ming
+Identify what aspects of the work have been correctly implemented and what have
+not. 
+	I believe I have implemented all the basic funcitons correctly, but I am
+not very sure because I was unable to confidently test my own code.
 
+Collaborators: Aaron Bowen, Becky Cutler, Daniel Baigel, and (of course) Ming
 
-Say approximately how many hours you have spent completing the assignment.
-Be written in either text format (README.txt) or in Markdown (README.md). No other formats will be accepted.
-For this assignment, you must also address the following questions:
+Time spent on assignment: 10 hours.
 
-Are the heuristics used in this assignment to determine incidents "even that good"?
-If you have spare time in the future, what would you add to the program or do differently with regards to detecting incidents?
+Questions: Are the heuristics used in this assignment to determine incidents
+"even that good"? 
+	I would argue that, yes, this alarm does have some merit. While it clearly 
+has its flaws (outlined further below), some alarm is better than no alarm, even 
+though it does detect a number of false positives as well as repetative alerts 
+for the same line (in the log analysis).
+
+If you have spare time in the future, what would you add to the program or do
+differently with regards to detecting incidents?      
+	It would be beneficial to find a way to scan for other nmap scans, credit 
+card leaks, and nikto scans without using match. Since match leaves a great deal 
+of room for variation (because it matches very generally), we can assume that 
+many of these alerts are actually false alarms. We also have repeat alarms, so 
+for reading logs it would be better to remove a line once an alarm has been 
+registered at that line. Searching for shellcode could also use improvement 
+because my matching or shell code was not particularlly extensive.
+
--- alarm/alarm.rb
+++ alarm/alarm.rb
@@ -2,12 +2,18 @@ require 'packetfu'
 require 'optparse'
 
 
+#Read live: scans a live stream of packets && looks for malicious scans
 def read_live
+	#incident number counter
+	i = 0
 	stream = PacketFu::Capture.new(:start => true, :iface => 'en0', :promisc => true)
 	#stream.show_live()
 	stream.stream.each do |raw|
 		pkt = PacketFu::Packet.parse(raw)
 		if pkt.is_tcp?
+			#update incident number
+			i= i + 1
+
 			#get the source IP address of the scan
 			sIP = pkt.ip_saddr
 
@@ -18,51 +24,94 @@ def read_live
 			p = protocol.last
 			
 			#NULL scan
-			if pkt.tcp_flags.urg == 0 and pkt.tcp_flags.ack == 0 and pkt.tcp_flags.psh == 0 and
-				pkt.tcp_flags.rst == 0 and pkt.tcp_flags.syn == 0 and pkt.tcp_flags.fin == 0
-				puts "ALERT: NULL scan is detected from " + sIP + " (" + p + ") " + "(" + pkt.payload + ")!"
+			if pkt.tcp_flags.urg == 0 && pkt.tcp_flags.ack == 0 && pkt.tcp_flags.psh == 0 &&
+				pkt.tcp_flags.rst == 0 && pkt.tcp_flags.syn == 0 && pkt.tcp_flags.fin == 0
+				puts i.to_s + ". ALERT: NULL scan is detected from " + sIP + " (" + p + ") " + "(" + pkt.payload + ")!"
 			end
 			
 			#FIN scan
-			if pkt.tcp_flags.urg == 0 and pkt.tcp_flags.ack == 0 and pkt.tcp_flags.psh == 0 and
-				pkt.tcp_flags.rst == 0 and pkt.tcp_flags.syn == 0 and pkt.tcp_flags.fin == 1
-				puts "ALERT: FIN scan is detected from " + sIP + " (" + p + ") " + "(" + pkt.payload + ")!"
+			if pkt.tcp_flags.urg == 0 && pkt.tcp_flags.ack == 0 && pkt.tcp_flags.psh == 0 &&
+				pkt.tcp_flags.rst == 0 && pkt.tcp_flags.syn == 0 && pkt.tcp_flags.fin == 1
+				puts i.to_s + ". ALERT: FIN scan is detected from " + sIP + " (" + p + ") " + "(" + pkt.payload + ")!"
 			end
 			
 			#Xmas scan
-			if pkt.tcp_flags.urg == 1 and pkt.tcp_flags.ack == 0 and pkt.tcp_flags.psh == 1 and
-				pkt.tcp_flags.rst == 0 and pkt.tcp_flags.syn == 0 and pkt.tcp_flags.fin == 1
-				puts "ALERT: XMAS scan is detected from " + sIP + " (" + p + ") " + "(" + pkt.payload + ")!"
+			if pkt.tcp_flags.urg == 1 && pkt.tcp_flags.ack == 0 && pkt.tcp_flags.psh == 1 &&
+				pkt.tcp_flags.rst == 0 && pkt.tcp_flags.syn == 0 && pkt.tcp_flags.fin == 1
+				puts i.to_s + ". ALERT: XMAS scan is detected from " + sIP + " (" + p + ") " + "(" + pkt.payload + ")!"
 			end				
 
 			#Other Nmap scans
 			if pkt.payload.scan(/\x4E\x6D\x61\x70/)
-				puts "ALERT: Nmap scan is detected from " + sIP + " (" + p + ") " + "(" + "(" + pkt.payload + ")!"
+				puts i.to_s + ". ALERT: Nmap scan is detected from " + sIP + " (" + p + ") " + "(" + "(" + pkt.payload + ")!"
 			end
 			
 			#Nikto scan 
 			if pkt.payload.scan(/\x4E\x69\x6B\x74\x6F\x0A/)
-				puts "ALERT: Nikto scan is detected from " + sIP + " (" + p + ") " + "(" + pkt.payload + ")!"
+				puts i.to_s + ". ALERT: Nikto scan is detected from " + sIP + " (" + p + ") " + "(" + pkt.payload + ")!"
 			end
 
 			#Credit card leak 
-			#{incident_number}. ALERT: #{incident} is detected from #{source IP address} (#{protocol}) (#{payload})!
+			if pkt.payload.scan(/4\d{3}(\s|-)?\d{4}(\s|-)?\d{4}(\s|-)?\d{4}/) || pkt.payload.scan(/5\d{3}(\s|-)?\d{4}(\s|-)?\d{4}(\s|-)?\d{4}/) || pkt.payload.scan(/6011(\s|-)?\d{4}(\s|-)?\d{4}(\s|-)?\d{4}/) || pkt.payload.scan(/3\d{3}(\s|-)?\d{6}(\s|-)?\d{5}/)
+				puts i.to_s + ". ALERT: Credit card leaked in the clear from " + sIP + " (" + p + ") " + "(" + pkt.payload + ")!"
+			end
 		end
 	end			
 end
 
+#Read log: takes a log input file && reports any incidents 
 def read_log (file)
-	line_num=0
+	#incident number counter
+	i = 0
+
 	text = File.open(file).read
 	#text.gsub!(/\r\n?/, "\n")
 	text.each_line do |line|
-		if line =~ /Nmap(.*)/
-			match = line.match(/^(.*?)(?=-)/)
-			ip = match.to_s
-			puts "ALERT: Nmap scan is detected from " + ip
+		#grab the ip address from the line
+			ipmatch = line.match(/^(.*?)(?=-)/)
+			ip = ipmatch.to_s
+
+		#find protocol
+			proto = ""
+			protomatch = line.match(/HTTP/)
+			proto = protomatch.to_s
+
+		#NMAP scan (2)
+		if /Nmap*/.match(line)
+			i = i + 1
+			puts i.to_s + ". ALERT: Nmap scan is detected from " + ip + "(" + proto + ") " + "(" + line + ")!"
+		end
+		
+		#Nikto scan (0)
+		if /Nikto*/.match(line)
+			i = i + 1
+			puts i.to_s + ". ALERT: Nikto scan is detected from " + ip + "(" + proto + ") " + "(" + line + ")!"
+		end
+
+		#Rob Graham's Masscan (19)
+		if line =~ /masscan*/
+			i = i + 1
+			puts i.to_s + ". ALERT: Someone is looking for masscan, scan is detected from " + ip + "(" + proto + ") " + "(" + line + ")!"
+		end
+
+		#Shellshock vulnerability
+		if line =~ /{ :;};echo/
+			i = i + 1
+			puts i.to_s + ". ALERT: Someone is looking for Shellshock vulnerability, scan is detected from " + ip + "(" + proto + ") " + "(" + line + ")!"
+		end
+
+		#phpMyAdmin
+		if line =~ /phpMyAdmin*/
+			i = i + 1
+			puts i.to_s + ". ALERT: Someone is looking for phpMyAdmin, scan is detected from " + ip + "(" + proto + ") " + "(" + line + ")!"
+		end
+
+		#other shellcode
+		if /xeb*/.match(line) || /xEB.*/.match(line) || /x16.*/.match(line) || /%2D%*/.match(line) || /perl*/.match(line)
+			i = i + 1
+			puts i.to_s + ". ALERT: Someone is trying to run shellcode, scan is detected from " + ip + "(" + proto + ") " + "(" + line + ")!"
 		end
 	end
-#{incident_number}. ALERT: #{incident} is detected from #{source IP address} (#{protocol}) (#{payload})!
 end
 
 
